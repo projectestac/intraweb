9b673ba001c6649f1098ecf1b705731c62ec8971
Toni Ginard <aginard@xtec.cat>
2014-01-24 13:35:20 +0100

    Uploaded maqueta v2.2 (Zikula 1.2)

---
+++ index.php
@@ -0,0 +1,1 @@
+<?php header("Location:intranet/index.php"); ?>
---
+++ intranet/install/step1.php
@@ -0,0 +1,4 @@
+<?php
+echo _STEP1;
+echo '<p><center><a href="index.php?step='.($step+1).'&lang='.$lang.'"><img src="images/next.png" width="24px" height="24px">'._GOON.'</a></center></p>';
+?>
